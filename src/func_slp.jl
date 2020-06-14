778412c55a0266381735f90f50d08ee5347d12c4
jjl3411 <justin.j.lee3411@gmail.com>
2020-06-13 20:54:31 -0500

    Debugged point estimation
    Debugged point estimation of local projection

--- src/func_slp.jl
+++ src/func_slp.jl
@@ -1,4 +1,24 @@
+mutable struct outputcom
+    T
+    H_min
+    H_max
+    HR
+    K
+    B
+    P
+    λ
+    type
+    δ
+    idx
+    Θ
+    IR
+    X
+    Y
+end
+
 function smoothlocalprojection(packagedinput)
+
+
     # Unpackage data
     y        = packagedinput.y;
     x        = packagedinput.x;
@@ -13,7 +33,7 @@ function smoothlocalprojection(packagedinput)
     if isempty(w)
         δ = std(x);
     else
-        δ = std( x .- w*inv(w'*w)*w'*x );
+        δ = std( x - w*inv(w'*w)*w'*x );
     end
 
     # Is it the regular regression-type?
@@ -25,27 +45,29 @@ function smoothlocalprojection(packagedinput)
 
     # construct the B-spline basis functions
     if ~isreg
-        B = bspline( (H_min:H_max)' , H_min , H_max+1 , H_max+1-H_min , 3 );
+        B = bspline((H_min:H_max)', H_min, (H_max + 1), (H_max+1-H_min), 3);
         K = size( B , 2 );
     else
         K = HR;
     end
 
     # building up the regression representation of the local projection
-    idx = nan( (H_max+1)*T , 2 );
-    Y   = nan( (H_max+1)*T , 1 );
-    Xb  = zeros( (H_max+1)*T , K );
-    Xc  = zeros( (H_max+1)*T , HR , size(w,2)+1 );
-
+    idx = nan((H_max+1)*T, 2);
+    Y   = nan((H_max+1)*T, 1);
+    Xb  = zeros(Float64, (H_max+1)*T, K);
+    Xc  = zeros(Float64, (H_max+1)*T, HR, size(w,2)+1 );
+    # NOTE: For potential for parallel computing, which may require to donwsize
+    #   bit size, will lead to substantially inaccurate results for inverse functions
+    # https://stackoverflow.com/questions/51562302/inverse-matrix-results-different-in-matlab-and-python
     w = hcat(ones(T,1), w);
 
-    for t = 1:(T - H_min)
+    for t ∈ 1:(T - H_min)
 
         idx_beg = (t - 1)*HR + 1;
         idx_end = t * HR;
 
-        idx[idx_beg:idx_end, 1] = t;
-        idx[idx_beg:idx_end, 2] = H_min:H_max;
+        idx[idx_beg:idx_end, 1] .= t;
+        idx[idx_beg:idx_end, 2] .= H_min:H_max;
 
         # y
         y_range = (t + H_min) : min((t + H_max), T)';
@@ -53,53 +75,99 @@ function smoothlocalprojection(packagedinput)
 
         # x
         if isreg
-            Xb[ idx_beg:idx_end , : ] = eye(HR)*x[t];
+            Xb[ idx_beg:idx_end , : ] = eye(HR) .* x[t];
         else
-            Xb[ idx_beg:idx_end , : ] = B*x[t];
+            Xb[ idx_beg:idx_end , : ] = B * x[t];
         end
 
         # w
-        for i = 1:size(w,2)
+        for i ∈ 1:size(w,2)
             Xc[idx_beg:idx_end , : , i ] = eye(HR)*w[t,i];
         end
-
     end
 
+    X = Xb;
+    for i ∈ 1:size(w,2)
+        X = hcat(X, Xc[:, :, i]);
+    end
 
-    # STOP line 59 of matlab
+    select = isfinite.(Y)[:];
+    idx = idx[select, :];
+    Y   = Y[select];
+    X   = X[select,:];
+
+    IR = zeros(Float16, H_max+1, 1);
+
+    if isreg
+        Θ = (X' * X)\X'*Y
+        IR[(H_min+1):end] = Θ[1:K] .* δ;
+
+        # Parameter not used but still defined,
+        B = 0;
+        P = zeros(Float16, size(X,2), size(X,2) );
+        λ = 0;
+    else
+        P = zeros(Float16, size(X,2), size(X,2));
+
+        D = eye(K);
+        for k = 1:r #k ∈ 1:r
+            D = diff(D, dims = 1);
+        end
+
+        P[1:K,1:K] = D' * D;
+
+        Θ = ( (X' * X) + (λ .* P) )\( X'*Y );
+
+        IR[(1+H_min):end] = B * Θ[1:K] .* δ;
+    end
+
+    output = outputcom(T, H_min, H_max, HR, K, B,P,λ,type,δ,idx,Θ,IR,X,Y) ;
+
+    return(output);
+    # debug stuff
+    # Bs is for display / debugging pourposes only
+    # Bs = bspline( (H_min:0.1:H)' , H_min , H+1 , H+1-H_min , 3 );
+    # obj.Bs = Bs;
 end
 
 function bspline(x, xl, xr, ndx, bdeg)
+
     dx = (xr - xl) / ndx;
 
-    t = xl .+ dx .* [-bdeg:(ndex - 1)];
-    T = (0 .*x + 1) .* t;
+    t = xl .+ dx .* vec(-bdeg:(ndx - 1))'
+    T = ones(Float16, length(x), 1) * t;
+    X = x' * ones(Float16, 1, length(t));
+    P = (X .- T) ./ dx;
 
-    X = x * (0 .* t .+ 1);
-    P = (X - T) / dx;
+    B = (T .<= X) .& (X .< (T .+ dx));
+    r = vcat(2:length(t), 1);
 
-    B = (T <= X) & (X < (T + dx));
-    r = [2:length(t) 1];
-
-    for k = 1:bdeg
-        B = (P .* B + (k + 1 - P) .* B(:, r)) / k;
+    for k ∈ 1:bdeg
+        B = (P .* B + (k + 1 .- P) .* B[:, r]) ./ k;
     end
     return B;
 end
 
-df_test = randn(20,7)
 function matlag(x, lag)
     t, k = size(x);
-    res = zeros((t-lag), lag * k)
-    for i = 1:lag
+    res = zeros(Float16, (t-lag), lag * k)
+
+    for i ∈ 1:lag
         res[:, ((i-1)*k + 1):(i*k)] = x[(lag-i + 1):end-i ,:]
     end
+
+    res = nan(t, lag * k);
+    for i ∈ 1:lag
+        lag_array = x[1:(end-i),:];
+        res[(i+1):end, ((i-1)*k + 1):(i*k)] = lag_array;
+    end
+
     return res
 end
 
-eye(n) = Matrix{Float64}(I, n, n)
+eye(n) = Matrix{Float16}(I, n, n)
 
 function nan(x, y)
-    temp = zeros(x, y);
+    temp = zeros(Float16, x, y);
     return replace(temp, 0 => NaN)
 end
--- src/main.jl
+++ src/main.jl
@@ -1,22 +1,23 @@
 using Pkg, Juno;
-using CSV, Statistics, LinearAlgebra: I;
+using CSV, DataFrames, Plots;
+using Statistics, SparseArrays, LinearAlgebra;
 cd(@__DIR__) #src
 
 include("func_slp.jl")
 
-mutable struct InputPackage
+mutable struct inputcom
   y        :: Array
   x        :: Array
   w        :: Array
   H_min    :: Int
   H_max    :: Int
   type     :: String
-  r        <: Float64
-  λ        <: Float64
+  r        :: Int8
+  λ        :: Int8
 end
 
-df = CSV.read("data.csv");
-df = convert(Array{Float64}, df[:, 2:4]);
+df = CSV.read("../test/data.csv");
+df = convert(Array{Float16}, df[:, 2:4]);
 T, k = size(df);
 
 P = 4; # Number of lags used in LP - controlled variable
@@ -26,31 +27,39 @@ H_min = 1;
 H_max = 20;
 
 # Indicators for variables
-ind_all = 1:k
+ind_all = 1:k;
 ind_response = 1; # Endogenous variable - response
 ind_shock    = 3; # Endogenous variable related to the shock
 
 ind_lp = "reg"; # Type of linear regression
 
 ind_contempo = filter(x -> x ≠ ind_shock, ind_all);
-
-y  = df[(P+1):end, ind_response]; # endogenous variable
-x  = df[(P+1):end, ind_shock]; # endoegnous variable related to the shock
+#,(P+1):end
+y  = df[:, ind_response]; # endogenous variable
+x  = df[:, ind_shock]; # endoegnous variable related to the shock
 
 # control variables (contemporaneous vars, lagged vars)
-w  = [ df[(P+1):end, ind_contempo]  matlag( df , P ) ];
-
+w  = [ df[:, ind_contempo]  matlag( df , P ) ];
+w[.!isfinite.(w)] .= 0;
 # Packaing everything as input
+r = 2; #(r-1)=order of the limit polynomial (so r=2 implies the IR is shrunk towards a line )
+λ = 100; # value of penalty
 
-testing = InputPackage(y, x, w, H_min, H_max,ind_lp, 3,4)
+testing₀ = inputcom(y, x, w, H_min, H_max, "reg", 3,4);
+testing₁ = inputcom(y, x, w, H_min, H_max, "smooth", r, λ);
 
-# Edit by 11/13/2019
+lp₀ = smoothlocalprojection(testing₀);
+#Juno.@enter smoothlocalprojection(testing₀);
+lp₁ = smoothlocalprojection(testing₁);
 
+plot(lp₀.IR)
+plot!(lp₁.IR)
 
-
-w( ~isfinite(w) ) = 0;
-
-lp = locproj(y,x,w,H_min,H_max,ind_lp); % IR from (standard) Local Projection
-
-r = 2; %(r-1)=order of the limit polynomial (so r=2 implies the IR is shrunk towards a line )
-lambda = 100; % value of penalty
+# Print responses in simple text format
+notes = ["C4", "D4", "E4", "F4"]
+outfile = "notes.txt"
+open(outfile, "w") do f
+  for i in notes
+    println(f, i)
+  end
+end # the file f is automatically closed after this block finishes
